e7fa2eb9fdb1f564bb80d6a4b90c8b4f6b760191
Andrew Beck <abeck@andrews-mac-pro.attlocal.net>
2017-01-20 21:29:09 -0500

    Add CMake (to make CLion happy), engine folder with start of app

---
+++ src/engine/ABApp.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Andrew Beck on 1/20/17.
+//
+
+#include "ABApp.h"
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+using namespace ZeroEngine;
+
+// This is the callback we'll be registering with GLFW for errors.
+// It'll just print out the error to the STDERR stream.
+void app_error_callback(int error, const char* description) {
+    fputs(description, stderr);
+}
+
+App::App(int windowWidth, int windowHeight, const char* windowTitle) {
+
+}
---
+++ src/engine/ABApp.h
@@ -0,0 +1,16 @@
+//
+// Created by Andrew Beck on 1/20/17.
+//
+
+#ifndef PROJECT_ABAPP_H
+#define PROJECT_ABAPP_H
+
+namespace ZeroEngine {
+    class App {
+        App(int windowWidth, int windowHeight, const char* windowTitle);
+
+
+    };
+}
+
+#endif //PROJECT_ABAPP_H
